453be067ef219667c42aa9a1084d09ad133c7427
Oleksii Huralnyk <ohuralnyk@lyft.com>
2021-10-14 11:25:26 +0300

    Initial Commit

---
+++ Shared/AudioOverBluetoohApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioOverBluetoohApp.swift
+//  Shared
+//
+//  Created by Oleksii Huralnyk on 14.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct AudioOverBluetoohApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
